06077988bee5280c2e703fbdb1d03cabae26fd77
berkay <berkay.avsar@protonmail.com>
2020-12-20 18:13:01 +0300

    d5.p1

---
+++ d5/find_seat.cpp
@@ -0,0 +1,70 @@
+#include <bitset>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long determine_row(const string &);
+long determine_col(const string &);
+
+int main(int argc, char **argv) {
+  ifstream input;
+  string line;
+  int seat_id = 0;
+  int highest_seat_id = 0;
+
+  if (argc < 2) {
+    cout << "Please enter a filename for input." << endl;
+    return 2;
+  }
+
+  input.open(argv[1]);
+
+  while (getline(input, line)) {
+    string row_part = line.substr(0, 7);
+    string col_part = line.substr(7, 3);
+
+    int row = determine_row(row_part);
+    int col = determine_col(col_part);
+
+    seat_id = (row * 8) + col;
+    if(seat_id > highest_seat_id) {
+      highest_seat_id = seat_id;
+    }
+  }
+
+  cout << "highest_seat: " << highest_seat_id << endl;
+}
+
+long determine_row(const string &line) {
+  bitset<7> row;
+  int i = 6;
+  for (auto &c : line) {
+    if (c == 'B') {
+      row[i] = 1;
+    } else if(c == 'F') {
+      row[i] = 0;
+    }
+    i--;
+  }
+  long retval = row.to_ulong();
+  return retval;
+}
+
+long determine_col(const string &line) { 
+  bitset<3> row;
+  int i = 2;
+  for (auto &c : line) {
+    if (c == 'R') {
+      row[i] = 1;
+    } else if (c == 'L') {
+      row[i] = 0;
+    }
+    i--;
+  }
+  long retval = row.to_ulong();
+  return retval;
+}
